af1b5e6511382ecac16be2077355133631ac141a
nathanwfish <nathanwfish@gmail.com>
2012-04-04 09:02:22 -0700

    fixed attendee calls

--- lib/go_to_meeting/attendees.rb
+++ lib/go_to_meeting/attendees.rb
@@ -2,15 +2,15 @@ module GoToMeeting
   module Attendees
     
     def get_attendees_by_group(group_key)
-      self.class.get("groups/#{group_key/attendees}").parsed_response
+      self.class.get("groups/#{group_key}/attendees").parsed_response
     end
     
     def get_attendees_by_meeting(meeting_key)
-      self.class.get("groups/#{group_key}/attendees}").parsed_response
+      self.class.get("groups/#{group_key}/attendees").parsed_response
     end
     
     def get_attendees_by_organizer(organizer_key)
-      self.class.get("organizers/#{organizer_key}/attendees}").parsed_response
+      self.class.get("organizers/#{organizer_key}/attendees").parsed_response
     end
     
   end
